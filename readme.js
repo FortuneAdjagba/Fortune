6bcf86917bb4b3bdc2e19b219bac28c3a5e5f42e
Fortune Adjagba <fortuneadjagba@gmail.com>
2024-08-19 13:24:47 +0000

    First Commit

--- readme.js
+++ readme.js
@@ -4,9 +4,37 @@ I am a software engineer...
 
 - 🙋🏻‍♂️ Always open to opportunities for collaboration and knowledge sharing
 - 🎓 Education: BSc in Software Engineering (Information Science)
-- 💻 Technologies: Python, JavaScript, Java, C++ / scikit-learn, Tensorflow
-- 📌 Currently learning: p5.js, Tensorflow
-- Find me on my [website](https://beacons.ai/mywebsite.com)
+- 💻 Technologies: Python, JavaScript / Generative AI, Tensorflow
+- 📌 Currently learning: p5.js, scikit-learn / Node.Js, VueJs 3 
+
+
+### Visitor count
+
+<img src="https://profile-counter.glitch.me/FortuneAdjagba/count.svg" />
+
+Added the `Wed 14 Sep 2022`
+
+Last update on Mon Aug 19 2024
+
+**135 day before 2025 ⏱** days before new years
+
+🤖 This README.md is updated with viciousness, by Fabot🤠
+
+Find me also on my [website](https://beacons.ai/mywebsite.com)
+
+
+<p align='left'>
+<details>
+
+ <summary><strong>Socials Media</strong></summary>
+
+  <a href="https://www.linkedin.com/in/fortune-adjagba/"><img height="24" src="https://github.com/FortuneAdjagba/FortuneAdjagba/blob/main/linkedin.png?raw=true"></a>&nbsp;&nbsp;
+  <a href="https://twitter.com/AdjagbaFortune"><img height="24" src="https://github.com/FortuneAdjagba/FortuneAdjagba/blob/main/twitter.png?raw=true"></a>&nbsp;&nbsp;
+  <a href="https://www.instagram.com/fortune_tg/"><img height="24" src="https://github.com/FortuneAdjagba/FortuneAdjagba/blob/main/instagram.png?raw=true"></a>&nbsp;&nbsp;
+  <a href="mailto:fortuneadjagba@gmail.com"><img height="24" src="https://github.com/FortuneAdjagba/FortuneAdjagba/blob/main/envelope.png?raw=true"></a>&nbsp;&nbsp;
+
+</details> 
+</p>
 
 ### Visitor count
 
